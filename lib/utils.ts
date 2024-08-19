c827b1e5d8f65a5491a68c27b9bc4af933a97d2e
Jagrut Manish Thakare <114098488+JagrutThakare@users.noreply.github.com>
2024-08-19 20:42:48 +0530

    New

--- lib/utils.ts
+++ lib/utils.ts
@@ -195,7 +195,17 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = z.object({
+export const authFormSchema = (type : string) => z.object({
+  //Sign-up
+  firstName: type=== 'sign-in'? z.string().optional() : z.string().min(3),
+  lastName: type=== 'sign-in'? z.string().optional() : z.string().min(3),
+  address1: type=== 'sign-in'? z.string().optional() : z.string(),
+  state: type=== 'sign-in'? z.string().optional() : z.string().min(3),
+  postalCode: type=== 'sign-in'? z.string().optional() : z.string().min(3).max(6),
+  dateOfBirth: type=== 'sign-in'? z.string().optional() : z.string().min(3),
+  ssn: type=== 'sign-in'? z.string().optional() : z.string().min(3),
+  
+  //Sign-in
   email: z.string().email(),
-  password: z.string().min(8)
+  password: z.string().min(8),
 })
